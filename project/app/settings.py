a2ceb619f5fb68ee2d7de697de8f88f143b6d9ba
hailoc12 <hailoc12@gmail.com>
2020-10-20 07:04:28 +0000

    fix bug export excel

--- project/app/settings.py
+++ project/app/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
