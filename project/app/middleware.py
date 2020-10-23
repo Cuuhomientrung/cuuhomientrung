5c3a083dedb823a8928d8ef5d6e13030834cf33e
DzungDA <dungda1616@gmail.com>
2020-10-24 02:07:34 +0700

    Admin can login, and anon user will auto login as "user1" name middleware class

--- project/app/middleware.py
+++ project/app/middleware.py
@@ -8,11 +8,13 @@ from django.middleware.csrf import CsrfViewMiddleware
 class AutomaticUserLoginMiddleware(MiddlewareMixin):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        # NOTE: Following code is to bypass login page. Change username to default login user you want
-        user = User.objects.get(username='admin')
-        # user = User.objects.get(username='admin')
-        request.user = user
-        auth.login(request, user)
+
+        if request.path == '/' and not request.user.is_authenticated:
+            # NOTE: Following code is to bypass login page. Change username to default login user you want
+            user = User.objects.get(username='user1')
+            # user = User.objects.get(username='admin')
+            request.user = user
+            auth.login(request, user)
 
 
 class RestAPICsrfMiddleware(CsrfViewMiddleware):
