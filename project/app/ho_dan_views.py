101a64f891e436d7e5acbc59035409cb27825454
dothanhlam97 <dothanhlam.98@gmail.com>
2020-10-23 09:47:48 +0100

    add pagination

--- project/app/ho_dan_views.py
+++ project/app/ho_dan_views.py
@@ -1,33 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from app.models import HoDan, HODAN_STATUS
 
 def get_ho_dan():
     return HoDan.objects.all()
 
+PAGE_SIZE = 2
+
 def index(request):
-    list_ho_dan = get_ho_dan()
+
     status_dict = dict(HODAN_STATUS)
-    context = {
-        'list_ho_dan': [
-            {
-                'id': ho_dan.id,
-                'name': ho_dan.name,
-                'update_time': ho_dan.update_time,
-                'tinh': ho_dan.tinh,
-                'huyen': ho_dan.huyen,
-                'xa': ho_dan.xa,
-                'location': ho_dan.location,
-                'status': status_dict[ho_dan.status],
-                'people_number': ho_dan.people_number,
-                'note': ho_dan.note,
-            }
-            for ho_dan in list_ho_dan
-        ],
-        'count': len(list_ho_dan),
-        'trang_thai_dict': {
-            0: "Chuaw",
-            1: "ma",
-        }
-    }
-    return render(request, 'ho_dan_index.html', context)
+
+    list_ho_dan = [{
+        'id': ho_dan.id,
+        'name': ho_dan.name,
+        'update_time': ho_dan.update_time,
+        'tinh': ho_dan.tinh,
+        'huyen': ho_dan.huyen,
+        'xa': ho_dan.xa,
+        'location': ho_dan.location,
+        'status': status_dict[ho_dan.status],
+        'people_number': ho_dan.people_number,
+        'note': ho_dan.note,
+        'phone': ho_dan.phone,
+    } for ho_dan in get_ho_dan()]
+
+    paginator = Paginator(list_ho_dan, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'ho_dan_index.html', {'page_obj': page_obj})
