7f45eefc98364416673cf2e2f341930bfa47031d
dungpt3003 <dungpt3003@gmail.com>
2020-10-25 22:40:26 +0000

    Add viewsets for tinhnguyenvien, xa, huyen, tinh

--- project/app/admin.py
+++ project/app/admin.py
@@ -7,7 +7,8 @@ from rest_framework import routers
 from app.settings import TIME_ZONE
 from app.models import TinTuc, TinhNguyenVien, CuuHo, HoDan, Tinh, Huyen, Xa,\
     TrangThaiHoDan, CUUHO_STATUS, TINHNGUYEN_STATUS
-from app.views import BaseRestfulAdmin, HoDanRestFulModelAdmin, CuuHoViewSet, HoDanViewSet
+from app.views import BaseRestfulAdmin, HoDanRestFulModelAdmin, CuuHoViewSet, HoDanViewSet,\
+    TinhNguyenVienViewSet, TinhViewSet, HuyenViewSet, XaViewSet
 from app.utils.export_to_excel import export_ho_dan_as_excel_action, utc_to_local
 from django.conf.locale.vi import formats as vi_formats
 from django_admin_listfilter_dropdown.filters import ChoiceDropdownFilter, RelatedDropdownFilter
@@ -402,6 +403,10 @@ admin.site.register(TrangThaiHoDan, TrangThaiHoDanAdmin)
 router = routers.DefaultRouter()
 router.register('cuuho', CuuHoViewSet)
 router.register('hodan', HoDanViewSet)
+router.register('tinhnguyenvien', TinhNguyenVienViewSet)
+router.register('tinh', TinhViewSet)
+router.register('huyen', HuyenViewSet)
+router.register('xa', XaViewSet)
 
 # rest_admin.site.register(
 #     HoDan, view_class=HoDanRestFulModelAdmin)
