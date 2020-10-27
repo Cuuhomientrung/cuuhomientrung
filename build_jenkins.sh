2c4c4b0066dfa7abc5d0876ea43b7565e9780f77
Tran Viet Dung <dungviettran89@gmail.com>
2020-10-27 22:41:01 +0800

    Add missing dependencies

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 set -e
-BUILD_NUMBER=${BUILD_NUMBER:=dev}
-VERSION=0.0.1
-IMAGE_TAG=$VERSION-$BUILD_NUMBER
-docker build -t cuuhomientrung/server:$IMAGE_TAG .
-docker push cuuhomientrung/server:$IMAGE_TAG
-docker tag cuuhomientrung/server:$IMAGE_TAG cuuhomientrung/server:latest
-docker push cuuhomientrung/server:latest
+docker build -t cuuhomientrung/server:latest .
