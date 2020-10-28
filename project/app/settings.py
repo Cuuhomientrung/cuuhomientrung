866d5692391de7b6f7a30232601f68d729c5a26f
DzungDA <dungda1616@gmail.com>
2020-10-29 01:30:13 +0700

    Use SECRET_KEY from env instead of hard code

--- project/app/settings.py
+++ project/app/settings.py
@@ -32,6 +32,7 @@ env = environ.Env(
     DEPLOY_ENV=(str, 'local'),
     GIT_VERSION=(str, None),
     CSRF_COOKIE_SECURE=(bool, False),
+    SECRET_KEY=(str, 'change_me'),
 )
 env.read_env(
     os.path.join(BASE_DIR, '..', '.env')
@@ -41,7 +42,7 @@ env.read_env(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ybcim6=@)la&g9@!asz1rx95=qd&39$tl1j1(1uflb_$mo*w##'
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
@@ -286,9 +287,6 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 REVISION = calendar.timegm(time.gmtime())
 
 SELECT2_USE_BUNDLED_JQUERY = False
-SELECT2_JS = 'admin/js/vendor/select2/select2.full.min.js'
-# SELECT2_CSS = 'admin/css/select2.css'
-SELECT2_I18N_PATH = 'admin/js/vendor/select2/i18n'
 CACHE_PREFIX = f'select2_cache_key_{REVISION}_'
 
 WEBPACK_LOADER = {
