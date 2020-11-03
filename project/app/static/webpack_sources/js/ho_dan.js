bb126b67235ccdabed2a2afdf2ab55d3ee0dc5e0
Phuc H <dshongphuc@gmail.com>
2020-11-03 22:19:04 +0700

    Make filters in ho_dan page searchable

--- project/app/static/webpack_sources/js/ho_dan.js
+++ project/app/static/webpack_sources/js/ho_dan.js
@@ -1,7 +1,16 @@
 import 'bootstrap';
 import $ from 'jquery';
+import Choices from 'choices.js'
+
 
 $(document).ready(() => {
+
+    // Init the choices.js instances :
+    let selectTinh = new Choices(document.getElementById('select-tinh'));
+    let selectHuyen = new Choices(document.getElementById('select-huyen'));
+    let selectXa = new Choices(document.getElementById('select-xa'));
+    let selectStatus = new Choices(document.getElementById('select-status'));
+
     $('#search-button').click(function() {
         let status = parseInt($("#select-status option:selected").val());
         let tinh = parseInt($("#select-tinh option:selected").val());
@@ -32,8 +41,24 @@ $(document).ready(() => {
                 let ele = $('<option></option>').attr("value", key).text(data[key]);
                 $('#select-huyen').append(ele);
             }
+
+            // Update the choices for Huyen :
+            // We have to update the choice as corrected array
+            let listHuyen = Object.keys(data).map(key => {
+                let item = {
+                    value: key,
+                    label: data[key]
+                }
+                return item
+            })
+
+            // Then update the choices:
+            selectHuyen.clearChoices()
+            selectHuyen.setChoices(listHuyen)
 		});
+
     });
+
     $('#select-huyen').on("change", function() {
         let huyen = $("#select-huyen option:selected").val();
         $.get('/get_xa_api/?huyen='+huyen, function(data, err) {
@@ -44,7 +69,22 @@ $(document).ready(() => {
                 let ele = $('<option></option>').attr("value", key).text(data[key]);
                 $('#select-xa').append(ele);
             }
+
+            // Update the choices for Xa
+            // We have to update the choice as corrected array
+            let listXa = Object.keys(data).map(key => {
+                let item = {
+                    value: key,
+                    label: data[key]
+                }
+                return item
+            })
+
+            // Then update the choices:
+            selectXa.clearChoices()
+            selectXa.setChoices(listXa)
 		});
+
     });
     $('#see-more-note').on("click", function() {
         $('.see-less').hide();
@@ -54,4 +94,8 @@ $(document).ready(() => {
         $('.see-less').show();
         $('.see-more').hide();
     });
+
+
+
+
 });
